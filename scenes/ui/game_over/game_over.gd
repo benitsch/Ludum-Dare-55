faec7c10117427b755f4f4dc3f9c4e56bd5f5604
Sebastian Theuermann <stheuerman@gmail.com>
2024-04-14 16:49:21 +0200

    Now displaying the number of souls at death in game over screen

--- scenes/ui/game_over/game_over.gd
+++ scenes/ui/game_over/game_over.gd
@@ -2,10 +2,12 @@ class_name GameOver
 extends Control
 
 @onready var music:AudioStreamPlayer = $Music
-@onready var back_button = $MarginContainer/HBoxContainer/VBoxContainer/Back_Button as Button
+@onready var back_button = $Back_Button as Button
+@onready var label_souls_statistic = $MarginContainer/HBoxContainer/VBoxContainer/LabelSoulsStatistic
 
 func _ready():
 	back_button.button_down.connect(on_back_pressed)
+	label_souls_statistic.text = "You managed to collect " + str(Autoload.souls) + " souls."
 	music.play()
 
 func on_back_pressed() -> void:
