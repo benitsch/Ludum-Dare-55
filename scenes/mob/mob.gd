1ba5e20c29549cf5f4a0032346ec3cdf848561ea
nitber <bernhardnitsch@gmx.at>
2024-04-14 16:25:37 +0200

    remove unused code; push not changing animation.txt

--- scenes/mob/mob.gd
+++ scenes/mob/mob.gd
@@ -41,24 +41,3 @@ func check_attack_area(delta):
 			if enemy.last_attack_time < 0.0:
 				enemy.last_attack_time = enemy.attack_speed
 				receive_damage(enemy.damage)
-
-#func _on_area_2d_body_entered(body):
-	#print("collide!")
-	## TODO show attack animation on player body (not e.g. on tree body) entered
-	#if body.has_method("receive_damage"):
-		#print("has receive damage func")
-		#timer.start()
-	#else:
-		#print("no receive_damage func")
-		#
-#func attack_player():
-	#print("mob call func attack_player")
-	#if player.is_alive:
-		#player.receive_damage(damage)
-	#else:
-		#timer.stop()
-	#
-
-
-func _on_attack_area_body_entered(_body):
-	pass # Replace with function body.
