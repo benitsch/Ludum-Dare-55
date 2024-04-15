920a8df7381b8594aadb79c22b1561012a9f8539
nitber <bernhardnitsch@gmx.at>
2024-04-15 09:28:21 +0200

    On ESC input key while playing, back to menu scene.

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,1 +1,10 @@
 extends Node2D
+
+
+func _process(_delta):
+	if Input.is_action_pressed("load_menu"):
+		Autoload.total_souls = 0
+		Autoload.souls = 0
+		Autoload.current_wave = 1
+		Autoload.wave_timer = 30
+		get_tree().change_scene_to_file("res://scenes/ui/main_menu/main_menu.tscn")
