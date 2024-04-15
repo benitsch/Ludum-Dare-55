d661dd5ef262e3c6240414eade4bb782a873a26e
RauscherTasha <natascharauscher@gmx.net>
2024-04-15 14:44:50 +0200

    add tutorial button

--- scenes/ui/main_menu/main_menu.gd
+++ scenes/ui/main_menu/main_menu.gd
@@ -2,12 +2,14 @@ class_name MainMenu
 extends Control
 
 @onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
+@onready var tutorial_button =  $MarginContainer/HBoxContainer/VBoxContainer/Tutorial_Button as Button
 @onready var credits_button = $MarginContainer/HBoxContainer/VBoxContainer/Credits_Button as Button
 @onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
 @onready var sfx_button_click = preload("res://assets/sfx/button_click.mp3")
 
 func _ready():
 	start_button.button_down.connect(on_start_pressed)
+	tutorial_button.button_down.connect(on_tutorial_pressed)
 	credits_button.button_down.connect(on_credits_pressed)
 	exit_button.button_down.connect(on_exit_pressed)
 	AutoloadAudioStreamPlayer.play_menu_music()
@@ -16,6 +18,11 @@ func on_start_pressed() -> void:
 	AutoloadAudioStreamPlayer.play_SFX(sfx_button_click)
 	AutoloadAudioStreamPlayer.play_level_music()
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+
+func on_tutorial_pressed() -> void:
+	AutoloadAudioStreamPlayer.play_SFX(sfx_button_click)
+	#AutoloadAudioStreamPlayer.play_level_music()
+	get_tree().change_scene_to_file("res://scenes/main/tutorial.tscn")
 
 func on_credits_pressed() -> void:
 	AutoloadAudioStreamPlayer.play_SFX(sfx_button_click)
